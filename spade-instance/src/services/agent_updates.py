201f3ade20a516c5e7b42068b10ae8aeec0522ef
madpeh <48161600+madpeh@users.noreply.github.com>
2022-12-15 05:29:48 +0100

    Work in progress

--- spade-instance/src/services/agent_updates.py
+++ spade-instance/src/services/agent_updates.py
@@ -5,6 +5,11 @@ from typing import TYPE_CHECKING
 from src.database.repositories.agent_updates import AgentUpdatesRepository
 from src.services.base import BaseServiceWithRepository
 
+if TYPE_CHECKING:
+    from typing import Any, Dict, List
+
+    from src.database.repositories.agent_updates import SavedAgentUpdates
+
 
 class AgentUpdatesService(BaseServiceWithRepository):
     repository_type = AgentUpdatesRepository
@@ -12,3 +17,8 @@ class AgentUpdatesService(BaseServiceWithRepository):
     @property
     def repository(self) -> repository_type:
         return self._repository
+
+    async def save_agent_updates(
+        self, batch: List[Dict[str, Any]]
+    ) -> SavedAgentUpdates:
+        return await self.repository.save_agent_updates(batch)
