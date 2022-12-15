411976211592065c2bf346551298f082b300186e
madpeh <48161600+madpeh@users.noreply.github.com>
2022-12-15 21:19:45 +0100

    Work in progress

--- translator/src/app.py
+++ translator/src/app.py
@@ -1,14 +1,11 @@
 from __future__ import annotations
 
-from aasm.utils.exception import PanicException
 from fastapi import FastAPI
 
-from src.exceptions import handle_panic_exception
 from src.routers import router
 
 
 def get_app() -> FastAPI:
     app = FastAPI()
     app.include_router(router)
-    app.add_exception_handler(PanicException, handle_panic_exception)
     return app
--- translator/src/routers.py
+++ translator/src/routers.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from aasm import __version__, get_spade_code
+from aasm import PanicException, __version__, get_spade_code
 from fastapi import APIRouter
+from starlette.responses import JSONResponse
 
 from src.models import AgentsAssemblyCode, PythonSpadeCode
 
@@ -10,8 +11,27 @@ router = APIRouter()
 
 @router.post("/python/spade", response_model=PythonSpadeCode, status_code=200)
 async def translate_aasm(agent_assembly_code: AgentsAssemblyCode):
-    spade_code = get_spade_code(agent_assembly_code.code_lines)
-    return PythonSpadeCode(
-        agent_code_lines=spade_code.agent_code_lines,
-        graph_code_lines=spade_code.graph_code_lines,
-    )
+    try:
+        spade_code = get_spade_code(agent_assembly_code.code_lines)
+        return PythonSpadeCode(
+            agent_code_lines=spade_code.agent_code_lines,
+            graph_code_lines=spade_code.graph_code_lines,
+        )
+    except PanicException as e:
+        return JSONResponse(
+            status_code=400,
+            content={
+                "translator_version": __version__,
+                "place": e.place,
+                "reason": e.reason,
+                "suggestion": e.suggestion,
+            },
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "translator_version": __version__,
+                "reason": str(e),
+            },
+        )
