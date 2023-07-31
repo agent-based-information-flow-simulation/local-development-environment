b63664cb89d237bab7019db60741a7ca31030726
Kajetan Rachwał <krachwal@ibspan.waw.pl>
2023-07-31 16:27:20 +0200

    Update translator and routers in spade-instance

--- spade-instance/src/services/translator.py
+++ spade-instance/src/services/translator.py
@@ -21,14 +21,20 @@ class TranslatedCode:
 
 
 class TranslatorService(BaseService):
-    async def translate(self, code: List[str]) -> TranslatedCode:
+    async def translate(self, aasm_code_lines: List[str], module_code_lines: List[List[str]]) -> TranslatedCode:
         url = f"{translator_settings.url}/python/spade"
+
+        joined_module_lines : List [str]= []
+        for mcl in module_code_lines:
+            joined_module_lines.extend(mcl)
+            joined_module_lines.append("\n")
+
         try:
             async with httpx.AsyncClient() as client:
                 response = await client.post(
                     url,
                     headers={"Content-Type": "application/json"},
-                    data=orjson.dumps({"code_lines": code}),
+                    data=orjson.dumps({"code_lines": aasm_code_lines, "module_lines": joined_module_lines})
                 )
         except Exception as e:
             raise TranslatorException(str(e))
