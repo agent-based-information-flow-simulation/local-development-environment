071642b6d966f5dc4d9f6fc253af8c8b19c0ef88
Kajetan Rachwał <krachwal@ibspan.waw.pl>
2023-09-05 21:53:30 +0200

    Add seed to graph generation

--- spade-instance/src/services/graph_creator.py
+++ spade-instance/src/services/graph_creator.py
@@ -17,17 +17,21 @@ class GraphCreatorService(BaseService):
     fake_domain: str = "x"
 
     def run_generated_algorithm(
-        self, graph_code_lines: List[str]
+        self, graph_code_lines: List[str], sim_id: str, seed: int
     ) -> List[Dict[str, Any]]:
         code_without_imports = self._remove_imports(graph_code_lines)
         exec("\n".join(code_without_imports))
         try:
-            algorithm: Callable[[str], List[Dict[str, Any]]] = locals()[
+            algorithm: Callable[[str, str, int], List[Dict[str, Any]]] = locals()[
                 "generate_graph_structure"
             ]
         except KeyError:
             raise GraphNotGeneratedException()
-        return algorithm(self.fake_domain)
+        if seed == -1:
+            seed = random.randint(0, 100000)
+        if sim_id == "":
+            sim_id = str(uuid.uuid4())
+        return algorithm(self.fake_domain, sim_id, seed)
 
     def _remove_imports(self, graph_code_lines: List[str]) -> List[str]:
         return list(
